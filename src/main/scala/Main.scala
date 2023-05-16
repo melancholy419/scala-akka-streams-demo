143fba23109c7cc4f4aaea01be6f96bd006652df
Babatunde <babatunde@leadremit.com>
2023-05-16 01:31:54 +0100

    removed the main class

--- src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-import scala.io.Source
-import java.io.File
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
-object Main extends App {
-  for (f <- Source.fromFile(new File("build.sbt")).getLines())
-    println(f)
-  println("Hello world")
-}
